4f306b74052bd61722ba2f83f71e9df87ec92a71
Hitesh Prajapat <142203204+Hitesh464156@users.noreply.github.com>
2024-09-30 18:47:56 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  output: "server",
+  site: "https://AnalyzeMyDream.com/",
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap(),
+    isProduction && partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ].filter(Boolean), 
+  adapter: netlify(),
+});
